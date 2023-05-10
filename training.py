262da108ce04f506fca91232f2cdd37ac4815261
Soma <somahansel@gmail.com>
2023-05-10 11:29:50 +0200

    x

--- training.py
+++ training.py
@@ -96,7 +96,8 @@ class LayerPersonalisationTrainingApp:
         self.totalTrainingSamples_count = 0
 
         self.models = self.initModels()
-        self.mergeModels(is_init=True)
+        if self.args.site_number > 1:
+            self.mergeModels(is_init=True)
         self.optims = self.initOptimizers()
         self.schedulers = self.initSchedulers()
 
