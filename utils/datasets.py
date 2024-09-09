c2cb12a6ceb37ad69967e5dce58253ab52622f55
Soma Hansel <hansel@john.iru.iam.loc>
2024-09-09 11:21:52 +0200

    cifar100 normalisation

--- utils/datasets.py
+++ utils/datasets.py
@@ -194,11 +194,11 @@ def get_cifar10_datasets(data_dir, use_hdf5=False):
     return dataset, val_dataset
 
 def get_cifar100_datasets(data_dir):
-    train_mean = [0.5071, 0.4866, 0.4409]
-    train_std = [0.2673, 0.2564, 0.2762]
+    train_mean = [0.4914, 0.4822, 0.4465]
+    train_std = [0.2470, 0.2435, 0.2616]
     train_transform = Compose([ToTensor(), Normalize(train_mean, train_std)])
-    val_mean = [0.5088, 0.4874, 0.4419]
-    val_std = [0.2683, 0.2574, 0.2771]
+    val_mean = [0.4914, 0.4822, 0.4465]
+    val_std = [0.2470, 0.2435, 0.2616]
     val_transform = Compose([ToTensor(), Normalize(val_mean, val_std)])
 
     dataset = CIFAR100(root=data_dir, train=True, download=True, transform=train_transform)
