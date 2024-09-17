8eb4b70c9f2f0aad672b9d5707ca763d8f618514
Soma <somahansel@gmail.com>
2024-09-17 10:41:21 +0200

    res save fix

--- main.py
+++ main.py
@@ -156,7 +156,7 @@ def new_main_plus_ft(logdir, comment, degradation, site_number, data_part_seed, 
         res['ft'] = ft_acc
     
     if len(res.keys()) > 0:
-        save_path = os.path.join('/home/hansel/developer/embedding/results', logdir)
+        save_path = os.path.join('/home/hansel/developer/embedding/results', logdir, comment) + '.pt'
         os.makedirs(save_path, exist_ok=True)
         res['strats'] = [trn_strategy, ft_strategy]
         torch.save(res, save_path)
