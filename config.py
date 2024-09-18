1c7a6f3ec7b400d3bc8a0479ae96251bbff438d6
Soma <somahansel@gmail.com>
2024-09-18 18:04:27 +0200

    shorter comment

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ def get_new_config(logdir, comment, site_number, degradation, comm_rounds, strat
     deg_string = ''.join([name[:2] for name in degradation]) if type(degradation) == list else degradation
 
     config = {'logdir':logdir,
-            'comment':f'{comment}-{strategy}-{model_type}-{deg_string}-s{str(site_number)}-fts{str(ft_site_number)}-b{str(batch_size)}-commr{str(comm_rounds)}-iter{str(iterations)}-lr1e-4-fflr{fflr}-elr{emb_lr}-ftelr{ft_emb_lr}-embdim-{emb_dim}-{dataset}-fedp-{str(fed_prox)}-proxm-{prox_map}-xval{cross_val_id}-gls{gl_seed}-nl-{norm_layer}-rst-{no_batch_running_stats}',
+            'comment':f'{comment}-{strategy}-{model_type}-{deg_string}-s{str(site_number)}-fts{str(ft_site_number)}-lr1e-4-fflr{fflr}-elr{emb_lr}-ftelr{ft_emb_lr}-embdim-{emb_dim}-{dataset}-fedp-{str(fed_prox)}-xval{cross_val_id}-gls{gl_seed}-nl-{norm_layer}',
             'task':task,
             'model_name':'resnet18',
             'model_type':model_type,
